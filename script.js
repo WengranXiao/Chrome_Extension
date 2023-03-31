3737820ce3fbcd69f34d2bb2d429d4b92258151c
WengranXiao <112583498+WengranXiao@users.noreply.github.com>
2023-03-31 16:02:57 -0400

    try1

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-xxx
+  const weatherData= async()=>{
+    const res =  await fetch("https://api.weatherapi.com/v1/current.json?key=83adb766ef6340dd9e8175134233103&q=48103&aqi=no")
+    const json = await res.json()
+   
+    document.getElementById("date").innerHTML = json.current.last_updated
+    document.getElementById("location").innerHTML = json.location.name
+    document.getElementById("temperature").innerHTML = json.current.temp_f
+    document.getElementById("weahter").innerHTML = json.current.condition.text
+  }
+  weatherData()
